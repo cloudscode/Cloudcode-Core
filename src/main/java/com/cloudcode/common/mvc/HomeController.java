74ccbd6c162d77f61b7fd660731144ba8bfbec6f
Administrator <Administrator@USER-20150923VB>
2016-06-02 15:59:29 +0800

    Signed-off-by: Administrator <Administrator@USER-20150923VB>

---
+++ src/main/java/com/cloudcode/common/mvc/HomeController.java
@@ -0,0 +1,59 @@
+package com.cloudcode.common.mvc;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+
+import javax.annotation.Resource;
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.octo.captcha.service.image.ImageCaptchaService;
+
+@Controller
+public class HomeController{
+	 protected final Logger logger = LoggerFactory.getLogger(HomeController.class);
+    @Resource
+    private ImageCaptchaService imageCaptchaService;
+    
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index(HttpServletRequest request, HttpServletResponse response) {
+        return "index";
+    }
+    
+    @RequestMapping(value = "/captcha", method = RequestMethod.GET)
+    public void getCode(HttpServletRequest request, HttpServletResponse response) {
+
+        try {
+            ByteArrayOutputStream jpegOutputStream = new ByteArrayOutputStream();
+            String captchaId = request.getSession().getId();
+            
+            BufferedImage challenge = (BufferedImage) imageCaptchaService.getChallengeForID(captchaId,request.getLocale());
+            
+            ImageIO.write(challenge, "jpeg", jpegOutputStream);
+            byte[] captchaChallengeAsJpeg = jpegOutputStream.toByteArray();
+            
+            response.setHeader("Cache-Control", "no-store");
+            response.setHeader("Pragma", "no-cache");
+            response.setDateHeader("Expires", 0);
+            response.setContentType("image/jpeg");
+            ServletOutputStream responseOutputStream = response.getOutputStream();
+            responseOutputStream.write(captchaChallengeAsJpeg);
+            responseOutputStream.flush();
+            responseOutputStream.close();
+            
+        } catch (Exception e) {
+            logger.error("generate captcha image error: {}", e.getMessage());
+        }
+        
+    }
+    
+}
+
