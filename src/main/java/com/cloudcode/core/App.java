0fe931c9e640eb0a8fc096574d682cfeb338fa82
lijian <ljzhuanjiao@gmail.com>
2016-06-06 20:41:21 +0800

    Signed-off-by: lijian <ljzhuanjiao@gmail.com>

--- src/main/java/com/cloudcode/core/App.java
+++ src/main/java/com/cloudcode/core/App.java
@@ -1,5 +1,9 @@
 package com.cloudcode.core;
 
+import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
+import org.springframework.security.authentication.encoding.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
+
 /**
  * Hello world!
  *
@@ -9,5 +13,16 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        StandardPasswordEncoder encoder = new StandardPasswordEncoder( "admin");
+        System.out.println();
+System.out.println(new Md5PasswordEncoder().encodePassword("000000", "admin"));
+
+
+Md5PasswordEncoder md5 = new Md5PasswordEncoder();       
+md5.setEncodeHashAsBase64(false);       
+       
+// 使用动态加密盐的只需要在注册用户的时候将第二个参数换成用户名即可       
+String pwd = md5.encodePassword("000000", "admin");       
+System.out.println("MD5 SystemWideSaltSource: " + pwd + " len=" + pwd.length());   
     }
 }
