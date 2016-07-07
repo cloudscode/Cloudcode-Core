7157cf48907c72bd61d390e1a6842661f5564cf2
via <ljzhuanjiao@gmail.com>
2016-07-07 14:51:16 +0800

    Signed-off-by: via <ljzhuanjiao@gmail.com>

--- src/main/java/com/cloudcode/task/aspect/EventAspect.java
+++ src/main/java/com/cloudcode/task/aspect/EventAspect.java
@@ -1,36 +1,60 @@
-/**
- * Project Name:Cloudcode-Core
- * File Name:EventAspect.java
- * Package Name:com.cloudcode.task.aspect
- * Date:2016-7-4下午5:05:23
- * Copyright (c) 2016, chenzhou1025@126.com All Rights Reserved.
- *
-*/
-
 package com.cloudcode.task.aspect;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-
-/**
- * ClassName:EventAspect <br/>
- * Function: TODO ADD FUNCTION. <br/>
- * Reason:	 TODO ADD REASON. <br/>
- * Date:     2016-7-4 下午5:05:23 <br/>
- * @author   cloudscode   ljzhuanjiao@Gmail.com 
- * @version  
- * @since    JDK 1.6
- * @see 	 
- */
+import org.springframework.stereotype.Component;
+ 
+@Component
 @Aspect
 public class EventAspect {
-	@Before("execution(* com.cloudcode.task.mvc.TaskConfigController.createTaskConfig(..))")
+	private static Log logger = LogFactory.getLog(EventAspect.class);
+	
+	/*@Pointcut("execution(public * com.cloudcode.task.dao.TaskConfigDao.addTaskConfig(..))")
+    public void myMethod(){};
+
+    @Before("execution(public void com.bjsxt.dao.impl.UserDAOImpl.save(com.bjsxt.model.User))")
+    @Before("myMethod()")
+    public void before() {
+        System.out.println("method staet");
+    } 
+    @After("myMethod()")
+    public void after() {
+        System.out.println("method after");
+    } */
+    
+	@Before("execution(public void com.cloudcode.task.dao.TaskConfigDao.addTaskConfig(..))")
 	public void logBefore(JoinPoint joinPoint) {
-		System.out.println("**************************************************");
+		System.out.println("*********************addTaskConfig****@Before*************************");
 		System.out.println("logBefore() is running!");
 		System.out.println("hijacked : " + joinPoint.getSignature().getName());
 		System.out.println("**************************************************");
+		logger.error("***********************************");
+	}
+	@After("execution(* com.cloudcode.task.dao.TaskConfigDao.addTaskConfig(..))")
+    public void logAfterV1(JoinPoint joinPoint) 
+    {
+		logger.error("*************addTaskConfig*@After********************");
+        System.out.println("EmployeeCRUDAspect.logAfterV1() : " + joinPoint.getSignature().getName());
+        logger.error("***********************************");
+    }
+	
+	@After("execution(* com.cloudcode.task.dao.TaskConfigDao.createTaskConfig(..))")
+    public void logAfterV4(JoinPoint joinPoint) 
+    {
+		logger.error("*****************createTaskConfig*@After****************");
+        System.out.println("EmployeeCRUDAspect.logAfterV1() : " + joinPoint.getSignature().getName());
+    }
+	@Before("execution(* com.cloudcode.task.dao.TaskConfigDao.createTaskConfig(..))")
+	public void logBefore5(JoinPoint joinPoint) {
+		System.out.println("*********************createTaskConfig******@Before***********************");
+		System.out.println("logBefore() is running!");
+		System.out.println("hijacked : " + joinPoint.getSignature().getName());
+		System.out.println("**************************************************");
+		logger.error("***********************************");
 	}
 }
 
