281dc2a145da598fe4bc558070a3193172fc6395
via <ljzhuanjiao@gmail.com>
2016-07-08 17:20:30 +0800

    Signed-off-by: via <ljzhuanjiao@gmail.com>

--- src/main/java/com/cloudcode/common/util/interceptor/DAOInterceptor.java
+++ src/main/java/com/cloudcode/common/util/interceptor/DAOInterceptor.java
@@ -6,6 +6,8 @@ import org.hibernate.EmptyInterceptor;
 
 
 public class DAOInterceptor extends EmptyInterceptor implements Serializable {
+ 
+	private static final long serialVersionUID = 8289755357462077893L;
 
 }
 
