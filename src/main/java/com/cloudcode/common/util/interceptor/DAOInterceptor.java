b0269ef96a4cbbf478e2c92b46a509bec4028103
via <ljzhuanjiao@gmail.com>
2016-07-08 17:14:19 +0800

    Signed-off-by: via <ljzhuanjiao@gmail.com>

---
+++ src/main/java/com/cloudcode/common/util/interceptor/DAOInterceptor.java
@@ -0,0 +1,11 @@
+package com.cloudcode.common.util.interceptor;
+
+import java.io.Serializable;
+
+import org.hibernate.EmptyInterceptor;
+
+
+public class DAOInterceptor extends EmptyInterceptor implements Serializable {
+
+}
+
